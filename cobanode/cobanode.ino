96e6d7141bc180b8448d48a43daf2848a28a4e17
irfanbstr <irfanbstr@gmail.com>
2018-03-23 22:38:12 +0700

    Add files via upload

---
+++ cobanode/cobanode.ino
@@ -0,0 +1,14 @@
+ #define blinkPin D1
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(blinkPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(blinkPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(blinkPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
